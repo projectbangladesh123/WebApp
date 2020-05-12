0696b48495d84a2fb33a517a3645e3000cf20e06
shafirpl <shafi.rpl@gmail.com>
2020-05-12 03:09:35 -0700

    correction

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/email", emailRoute);
 
-app.use(express.static("client/build"));
+app.use(express.static("frontend/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
 
